0c2e0e305d7b4d412cbf6139b8630785baf7204b
Aleksandra Kucharska <aleksandra0201@gmail.com>
2020-04-22 22:27:38 +0100

    Separate draw function into draw and ballSetup function to make code cleaner

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const ctx = canvas.getContext('2d');
 
 const ballColour = '#0095dd';
 
-function draw() {
+var xPosition = canvas.width / 2;
+var yPosition = canvas.height - 30;
+
+var xMovement = 2;
+var yMovement = -2;
+
+function ballSetup() {
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.arc(xPosition, yPosition, 10, 0, Math.PI*2);
     ctx.fillStyle = ballColour;
     ctx.fill();
     ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ballSetup();
+    xPosition += xMovement;
+    yPosition += yMovement;
 }
 
 setInterval(draw, 10);
