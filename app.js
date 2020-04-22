ed43a859bc84eb8c851e4946edf263772251a07a
Aleksandra Kucharska <aleksandra0201@gmail.com>
2020-04-22 21:38:22 +0100

    Set up a ball, redraw it with setInterval()

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const canvas = document.querySelector('#gameCanvas');
 const ctx = canvas.getContext('2d');
+
+const ballColour = '#0095dd';
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = ballColour;
+    ctx.fill();
+    ctx.closePath();
+}
+
+setInterval(draw, 10);
